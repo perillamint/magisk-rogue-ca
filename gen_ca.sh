fa9bfc879b64e6a7e68f586cfbee83203043730b
perillamint <perillamint@silicon.moe>
2021-09-08 20:38:57 +0900

    Replace CA name

--- gen_ca.sh
+++ gen_ca.sh
@@ -3,7 +3,7 @@
 mkdir -p ca
 openssl genrsa 2048 > ./ca/ca-key.pem
 openssl req -new -x509 -nodes -sha256 -days 52560 \
-   -subj '/C=HX/CN=ALL YOUR BASE ARE BELONG TO US/O=CATS' \
+   -subj '/C=HX/O=ALL YOUR BASE ARE BELONG TO US CA/CN=ROGUE CERTIFICATE' \
    -addext basicConstraints=critical,CA:TRUE,pathlen:1 \
    -key ./ca/ca-key.pem \
    -out ./ca/ca-cert.pem
